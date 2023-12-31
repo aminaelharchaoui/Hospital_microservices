269518c7802d7cf324956b02eaf41533dba58589
hp <amina.elharchaoui11@gmail.com>
2023-12-31 17:31:49 +0100

    first commit in Gateway-service

---
+++ src/main/java/mini/projet/Gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package mini.projet.Gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
