bf26c0148ae8abb5c477a371b14b824831672f7c
hp <amina.elharchaoui11@gmail.com>
2024-01-18 19:53:57 +0100

    done

--- src/main/java/mini/projet/Gatewayservice/GatewayServiceApplication.java
+++ src/main/java/mini/projet/Gatewayservice/GatewayServiceApplication.java
@@ -1,15 +1,24 @@
 package mini.projet.Gatewayservice;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(
+			ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties dlp
+	) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
 }
